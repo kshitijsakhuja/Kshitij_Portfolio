b25f93ab8626cc5f06505ef8a8f198a234db9882
Kshitij Sakhuja <118502165+kshitijsakhuja@users.noreply.github.com>
2024-08-09 10:00:33 +0530

    new

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Data Analyst","Web Developer"],
+        strings: [ "Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Data Analyst","Web Developer"],
+        strings: [ "Data Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
